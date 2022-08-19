61394afd8f6e9c7e78109b4b81a3b3db1243d5a6
Kaike Yudi <kaikeyudi@outlook.com>
2022-08-19 15:36:59 -0300

    req-8

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -17,58 +17,86 @@ const { expect } = chai;
 describe('POST /login', () => {
   let chaiHttpResponse: Response;
 
-  before(async () => {
-    sinon
-      .stub(UserModel, "findOne")
-      .resolves({
-        id: 1,
-        username: "testUsername",
-        role: "testRole",
-        email: "test@email.com",
-        password: bcryptjs.hashSync('testPassword'),
-      } as UserModel);
+  describe('BAD_REQUEST', () => {
+    before(async () => {
+      sinon
+        .stub(UserModel, "findOne")
+        .resolves({
+          id: 1,
+          username: "testUsername",
+          role: "testRole",
+          email: "test@email.com",
+          password: bcryptjs.hashSync('testPassword'),
+        } as UserModel);
+      });
+  
+    after(()=>{
+      (UserModel.findOne as sinon.SinonStub).restore();
+    })
+  
+    it('succeeds', async () => {
+      chaiHttpResponse = await chai.request(app)
+        .post('/login')
+        .send({
+          email: "test@email.com",
+          password: "testPassword"
+        });
+  
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.be.a('object');
+      expect(chaiHttpResponse.body).to.have.a.property('token');
     });
-
-  after(()=>{
-    (UserModel.findOne as sinon.SinonStub).restore();
+  
+    it('fails with email missing', async () => {
+      chaiHttpResponse = await chai.request(app)
+        .post('/login')
+        .send({
+          password: "testPassword"
+        });
+  
+      const { body: { message } } = chaiHttpResponse
+  
+      expect(chaiHttpResponse).to.have.status(400);
+      expect(message).to.be.deep.equal('All fields must be filled');
+    });
+  
+    it('fails with password missing', async () => {
+      chaiHttpResponse = await chai.request(app)
+        .post('/login')
+        .send({
+          email: "test@email.com"
+        });
+  
+      const { body: { message } } = chaiHttpResponse
+  
+      expect(chaiHttpResponse).to.have.status(400);
+      expect(message).to.be.deep.equal('All fields must be filled');
+    });
   })
 
-  it('succeeds', async () => {
-    chaiHttpResponse = await chai.request(app)
-      .post('/login')
-      .send({
-        email: "test@email.com",
-        password: "testPassword"
+  describe('UNAUTHORIZED', () => {
+    before(async () => {
+      sinon
+        .stub(UserModel, "findOne")
+        .resolves(null);
       });
+  
+    after(()=>{
+      (UserModel.findOne as sinon.SinonStub).restore();
+    })
 
-    expect(chaiHttpResponse).to.have.status(200);
-    expect(chaiHttpResponse.body).to.be.a('object');
-    expect(chaiHttpResponse.body).to.have.a.property('token');
-  });
-
-  it('fails with email missing', async () => {
-    chaiHttpResponse = await chai.request(app)
-      .post('/login')
-      .send({
-        password: "testPassword"
-      });
-
-    const { body: { message } } = chaiHttpResponse
-
-    expect(chaiHttpResponse).to.have.status(400);
-    expect(message).to.be.deep.equal('All fields must be filled');
-  });
-
-  it('fails with password missing', async () => {
-    chaiHttpResponse = await chai.request(app)
-      .post('/login')
-      .send({
-        email: "test@email.com"
-      });
-
-    const { body: { message } } = chaiHttpResponse
-
-    expect(chaiHttpResponse).to.have.status(400);
-    expect(message).to.be.deep.equal('All fields must be filled');
-  });
+    it('fails with incorrect email', async () => {
+      chaiHttpResponse = await chai.request(app)
+        .post('/login')
+        .send({
+          email: "email@test.com",
+          password: "testPassword"
+        });
+  
+      const { body: { message } } = chaiHttpResponse
+  
+      expect(chaiHttpResponse).to.have.status(401);
+      expect(message).to.be.deep.equal('Incorrect email or password');
+    });
+  })
 });
