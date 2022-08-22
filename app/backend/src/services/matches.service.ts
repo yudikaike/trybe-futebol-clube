2d0de7d3cd1e76afebb4b6ff37124efa31fff010
Kaike Yudi <kaikeyudi@outlook.com>
2022-08-22 10:22:56 -0300

    feat: implementa validação para o endpoint /matches

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,6 +1,7 @@
 import TeamModel from '../database/models/team.model';
 import MatchModel from '../database/models/match.model';
 import { Match } from '../interfaces';
+import error from '../middlewares';
 
 export default class MatchesService {
   static list(query: boolean) {
@@ -18,6 +19,10 @@ export default class MatchesService {
   }
 
   static async add({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals }: Match) {
+    if (homeTeam === awayTeam) {
+      throw error
+        .custom('UnauthorizedError', 'It is not possible to create a match with two equal teams');
+    }
     return MatchModel.create({
       homeTeam,
       awayTeam,
